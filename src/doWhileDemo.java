0df459b7ebce8528d339f12657ce809a0050430e
ASUS <ASUS@lcc>
2017-09-04 15:22:48 +0800

    teacher's sample

---
+++ src/doWhileDemo.java
@@ -0,0 +1,14 @@
+public class doWhileDemo {
+	public static void main(String[] args) {
+		java.util.Scanner scanner = 
+			new java.util.Scanner(System.in);
+
+		int input = 0;
+		do{
+			System.out.print("輸入數字: ");
+			input = scanner.nextInt();
+		} while(input!=30);
+
+		System.out.println("恭喜您!猜對了!");
+	}
+}
---
+++ src/whilevariable.java
@@ -0,0 +1,9 @@
+public class whilevariable{
+    public static void main(String[] args)   {
+       double r = 0;
+       while(r < 0.99d) {
+            r = Math.random(); // Math 類別中提供產生亂數的類別方法
+            System.out.println(r);
+	                 } // end of while
+   }
+}
