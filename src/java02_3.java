356bee0afd5437ba97d4ef9adcdf2ac01d39049c
ASUS <ASUS@lcc>
2017-09-07 12:15:15 +0800

    溫度轉換器

---
+++ src/java02_3.java
@@ -0,0 +1,18 @@
+
+
+public class java02_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*------------------放锣传竟--------------------*/
+		int C = 100, F = 100;// 尼ん地ん
+
+		// 地ん = 尼ん*9/5+32
+		System.out.println("尼ん100=地ん"+(C*9/5+32)+"");
+		// 尼ん = (地ん-32)*5/9
+		System.out.println("地ん100=尼ん"+((F-32)*5/9)+"");
+
+		
+	}
+
+}
