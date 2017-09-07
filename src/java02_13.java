3ab039bf388c0127583890d8ed0dad6d583d8fce
ASUS <ASUS@lcc>
2017-09-07 13:34:16 +0800

    亂數矩陣練習

--- src/java02_13.java
+++ src/java02_13.java
@@ -1,17 +1,6 @@
 public class java02_13 {
 	public static void main(String[] args) {
-		String[] title = { "編號", "價格", "庫存量" };
-/*		int[][] bookShop = { { 1, 2, 3 }, { 200, 350, 250 }, { 5, 3, 10 } };
-
-		for (int i = 0; i < bookShop.length; i++) {
-			System.out.print(title[i] + "\t");
-
-			for (int j = 0; j < bookShop[i].length; j++)
-				System.out.print(bookShop[i][j] + "\t");
-			System.out.println();
-		}
-*/
-		
+	
 		//第一組矩陣
 		int[][] numbergame_a = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
@@ -55,10 +44,12 @@ public class java02_13 {
 		}
 		
 		
-		//進階題
+		//--------------------進階題-----------------------
 		
+		
+		//亂數矩陣一
 		System.out.println();
-		System.out.println("用亂數產生9個介於0-20的數字");
+		System.out.println("-----用亂數產生9個介於0-20的數字-----\n第一組亂數");
 		for (int i = 0; i < numbergame_a.length; i++) {
 
 			for (int j = 0; j < numbergame_a[i].length; j++) {
@@ -68,6 +59,33 @@ public class java02_13 {
 			System.out.println();
 
 		}
+		
+		//亂數矩陣二
+		System.out.println();
+		System.out.println("第二組亂數");
+		for (int i = 0; i < numbergame_b.length; i++) {
+
+			for (int j = 0; j < numbergame_b[i].length; j++) {
+				
+				System.out.print((numbergame_b[i][j] = (int) (Math.random() * 21)) + "\t");
+			}
+			System.out.println();
+
+		}
+		
+		//將亂數矩陣一、亂數矩陣二相加
+	 
+				System.out.println("\n亂數矩陣相加");
+				for (int i = 0; i < numbergame_c.length; i++) {
+
+					for (int j = 0; j < numbergame_c[i].length; j++) {
+						
+						System.out.print((numbergame_c[i][j]=numbergame_a[i][j]+numbergame_b[i][j]) + "\t");
+					}
+					System.out.println();
+
+				}
+		
 		
 	}
 }
